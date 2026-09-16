6192cc6a5c5b17617be35dde00ee990442fb1626
dj720 <dj720@users.noreply.github.com>
2026-09-16 02:14:07 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict Ypit6eT1oGmZBSVgvwViWOA0qAYvpqMw5939YfczR3Ir25PpCEGVsc4zxY5jWIm
+-- \restrict 3wCwPNx5tJP1xybs7WJduPYt6qmnQZSToSyNleW02xFkHIPOY2AtwDEHmDN7LIm
 
 -- Dumped from database version 15.8
 -- Dumped by pg_dump version 17.6
@@ -2029,6 +2029,22 @@ COPY "auth"."mfa_challenges" ("id", "factor_id", "created_at", "verified_at", "i
 
 
 --
+-- Data for Name: mfa_recovery_code_sets; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+COPY "auth"."mfa_recovery_code_sets" ("id", "user_id", "mfa_factor_id", "failed_verification_count", "verification_locked_until", "created_at", "updated_at") FROM stdin;
+\.
+
+
+--
+-- Data for Name: mfa_recovery_codes; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+COPY "auth"."mfa_recovery_codes" ("id", "mfa_recovery_code_set_id", "code_hash", "consumed_at", "created_at") FROM stdin;
+\.
+
+
+--
 -- Data for Name: oauth_authorizations; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
@@ -2056,8 +2072,8 @@ COPY "auth"."oauth_consents" ("id", "user_id", "client_id", "scopes", "granted_a
 -- Data for Name: one_time_tokens; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-COPY "auth"."one_time_tokens" ("id", "user_id", "token_type", "token_hash", "relates_to", "created_at", "updated_at") FROM stdin;
-f0141082-3818-4839-84e0-c2428ee55fbc	f4f2721b-d4bb-4c6b-bdab-da64efdce650	recovery_token	e7ecec3108e5350f39fb22e4c71bb240be33f3c9a7a6ef31b9759811	djn1@live.co.uk	2026-01-04 14:14:59.782017	2026-01-04 14:14:59.782017
+COPY "auth"."one_time_tokens" ("id", "user_id", "token_type", "token_hash", "relates_to", "created_at", "updated_at", "expires_at") FROM stdin;
+f0141082-3818-4839-84e0-c2428ee55fbc	f4f2721b-d4bb-4c6b-bdab-da64efdce650	recovery_token	e7ecec3108e5350f39fb22e4c71bb240be33f3c9a7a6ef31b9759811	djn1@live.co.uk	2026-01-04 14:14:59.782017	2026-01-04 14:14:59.782017	\N
 \.
 
 
@@ -2115,6 +2131,22 @@ COPY "auth"."saml_providers" ("id", "sso_provider_id", "entity_id", "metadata_xm
 --
 
 COPY "auth"."saml_relay_states" ("id", "sso_provider_id", "request_id", "for_email", "redirect_to", "created_at", "updated_at", "flow_state_id") FROM stdin;
+\.
+
+
+--
+-- Data for Name: scim_tokens; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+COPY "auth"."scim_tokens" ("id", "sso_provider_id", "token_hash", "prefix", "created_at", "expires_at", "revoked_at", "last_used_at") FROM stdin;
+\.
+
+
+--
+-- Data for Name: scim_users; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+COPY "auth"."scim_users" ("id", "sso_provider_id", "user_id", "resource", "created_at", "updated_at", "deleted_at") FROM stdin;
 \.
 
 
@@ -2900,6 +2932,6 @@ SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 921, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict Ypit6eT1oGmZBSVgvwViWOA0qAYvpqMw5939YfczR3Ir25PpCEGVsc4zxY5jWIm
+-- \unrestrict 3wCwPNx5tJP1xybs7WJduPYt6qmnQZSToSyNleW02xFkHIPOY2AtwDEHmDN7LIm
 
 RESET ALL;
